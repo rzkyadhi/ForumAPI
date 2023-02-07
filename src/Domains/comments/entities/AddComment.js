5dbbd6b2f06c554dbaef46a37ced6bf353bd4e85
rzkyadhi <rizkyadhi081@gmail.com>
2023-02-07 20:40:49 +0700

    Adding src/Domains/comments/entities/AddComment and TDD Tested

---
+++ src/Domains/comments/entities/AddComment.js
@@ -0,0 +1,27 @@
+class AddComment {
+    constructor(payload) {
+        this._verifyPayload(payload)
+        const {
+            thread_id,
+            content,
+            owner
+        } = payload
+
+        this.thread_id = thread_id
+        this.content = content
+        this.owner = owner
+    }
+
+    _verifyPayload({
+        thread_id,
+        content,
+        owner
+    }) {
+        if (!thread_id || !content || !owner)
+            throw new Error('ADD_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY')
+        if (typeof thread_id !== 'string' || typeof content !== 'string' || typeof owner !== 'string')
+            throw new Error('ADD_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION')
+    }
+}
+
+module.exports = AddComment
