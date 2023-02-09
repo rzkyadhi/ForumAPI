88cf2ec3cb226e9e26c6528eaecf15d3cdaa0b29
rzkyadhi <rizkyadhi081@gmail.com>
2023-02-10 06:57:21 +0700

    Adding tests/CommentsTableTestHelper to help for testing on comments table

---
+++ tests/CommentsTableTestHelper.js
@@ -0,0 +1,34 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool')
+
+const CommentsTableTestHelper = {
+    async addComment({
+        id = 'comment-123',
+        thread_id = 'thread-123',
+        content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        owner = 'owner-123'
+    }) {
+        const query = {
+            text: 'INSERT INTO comments VALUES($1, $2, $3, $4)',
+            values: [id, thread_id, content, owner]
+        }
+
+        await pool.query(query)
+    },
+
+    async findCommentById(id) {
+        const query = {
+            text: 'SELECT * FROM comments WHERE id = $1',
+            values: [id]
+        }
+
+        const result = await pool.query(query)
+        return result.rows
+    },
+
+    async cleanTable()  {
+        await pool.query('DELETE FROM comments WHERE 1=1')
+    }
+}
+
+module.exports = CommentsTableTestHelper
