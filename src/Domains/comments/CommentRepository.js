5921aa41fe3cb16feac2d0f7ff8d190a305fe8a7
rzkyadhi <rizkyadhi081@gmail.com>
2023-02-11 07:05:42 +0700

    Adding src/Domains/comments/CommentRepository and TDD Tested

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,11 @@
+class CommentRepository {
+    async addComment(addComment) {
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+    }
+
+    async getCommentById(id) {
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+    }
+}
+
+module.exports = CommentRepository
