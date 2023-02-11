e830879ef9660bae0f83fb6118b29b8d48827f33
rzkyadhi <rizkyadhi081@gmail.com>
2023-02-11 11:17:10 +0700

    Adding src/Applications/use_case/AddCommentUseCase and TDD Tested

---
+++ src/Applications/use_case/AddCommentUseCase.js
@@ -0,0 +1,30 @@
+const AddComment = require('../../Domains/comments/entities/AddComment')
+
+class AddCommentUseCase {
+    constructor({
+        threadRepository,
+        commentRepository,
+        authenticationTokenManager
+    }) {
+        this._threadRepository = threadRepository
+        this._commentRepository = commentRepository
+        this._authenticationTokenManager = authenticationTokenManager
+    }
+
+    async execute(useCasePayload, bearerToken, useCaseParam) {
+        const threadId = await this._threadRepository.getThreadById(useCaseParam.threadId)
+        const accessToken = await this._authenticationTokenManager.getTokenFromHeader(bearerToken)
+        await this._authenticationTokenManager.verifyAccessToken(accessToken)
+        const {
+            id: owner
+        } = await this._authenticationTokenManager.decodePayload(accessToken)
+        const addComment = new AddComment({
+            thread_id: threadId,
+            ...useCasePayload,
+            owner: owner
+        })
+        return this._commentRepository.addComment(addComment)
+    }
+}
+
+module.exports = AddCommentUseCase
---
+++ src/Applications/use_case/_test/AddCommentUseCase.test.js
@@ -0,0 +1,67 @@
+const AddComment = require('../../../Domains/comments/entities/AddComment')
+const AddedComment = require('../../../Domains/comments/entities/AddedComment')
+const CommentRepository = require('../../../Domains/comments/CommentRepository')
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository')
+const AuthenticationTokenManager = require('../../security/AuthenticationTokenManager')
+const AddCommentUseCase = require('../AddCommentUseCase')
+
+describe('AddCommentUseCase', () => {
+    it('should orchestrating the add comment action correctly', async () => {
+        // Arrange
+        const useCasePayload = {
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        }
+        const expectedAddedComment = new AddedComment({
+            id: 'comment-123',
+            content: useCasePayload.content,
+            owner: 'owner-123'
+        })
+        const bearerToken = 'Bearer accessToken'
+        const accessToken = 'accessToken'
+        const useCaseParam = {
+            threadId: 'thread-123'
+        }
+
+        /* creating dependency of use case */
+        const mockThreadRepository = new ThreadRepository()
+        const mockCommentRepository = new CommentRepository()
+        const mockAuthenticationTokenManager = new AuthenticationTokenManager()
+
+        /* mocking needed function */
+        mockThreadRepository.getThreadById = jest.fn()
+            .mockImplementation(() => Promise.resolve(useCaseParam.threadId))
+        mockAuthenticationTokenManager.getTokenFromHeader = jest.fn()
+            .mockImplementation(() => Promise.resolve(accessToken))
+        mockAuthenticationTokenManager.verifyAccessToken = jest.fn()
+            .mockImplementation(() => Promise.resolve())
+        mockAuthenticationTokenManager.decodePayload = jest.fn()
+            .mockImplementation(() => Promise.resolve({
+                id: expectedAddedComment.owner
+            }))
+        mockCommentRepository.addComment = jest.fn()
+            .mockImplementation(() => Promise.resolve(expectedAddedComment))
+
+        /* creating use case instance */
+        const addCommentUseCase = new AddCommentUseCase({
+            threadRepository: mockThreadRepository,
+            commentRepository: mockCommentRepository,
+            authenticationTokenManager: mockAuthenticationTokenManager
+        })
+
+        // Action
+        const addedComment = await addCommentUseCase.execute(useCasePayload, bearerToken, useCaseParam)
+
+
+        // Assert
+        expect(addedComment).toStrictEqual(expectedAddedComment)
+        expect(mockThreadRepository.getThreadById).toBeCalledWith(useCaseParam.threadId)
+        expect(mockAuthenticationTokenManager.getTokenFromHeader).toBeCalledWith(bearerToken)
+        expect(mockAuthenticationTokenManager.verifyAccessToken).toBeCalledWith(accessToken)
+        expect(mockAuthenticationTokenManager.decodePayload).toBeCalledWith(accessToken)
+        expect(mockCommentRepository.addComment).toBeCalledWith(new AddComment({
+            thread_id: useCaseParam.threadId,
+            content: useCasePayload.content,
+            owner: expectedAddedComment.owner
+        }))
+    })
+})
