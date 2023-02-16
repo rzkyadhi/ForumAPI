ee1f4e9c2fb7fdddb3ab5bbf433a1d540037e3c4
rzkyadhi <rizkyadhi081@gmail.com>
2023-02-17 06:44:29 +0700

    Adding migrations/1676590732389_add-column-is-deleted-in-comments-table

---
+++ migrations/1676590732389_add-column-is-deleted-in-comments-table.js
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.addColumns('comments', {
+        is_deleted: {
+            type: 'BOOLEAN',
+            notNull: true,
+            default: false
+        }
+    })
+};
+
+exports.down = pgm => {
+    pgm.dropColumns('comments', 'is_deleted')
+};
