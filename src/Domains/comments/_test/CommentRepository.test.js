e52b9a862b455a458f404100eafac00fefe5711b
rzkyadhi <rizkyadhi081@gmail.com>
2023-02-26 09:33:06 +0700

    Editing src/Domains/comments/CommentRepository and TDD Tested

--- src/Domains/comments/_test/CommentRepository.test.js
+++ src/Domains/comments/_test/CommentRepository.test.js
@@ -8,5 +8,8 @@ describe('CommentRepository interface', () => {
         // Action and Assert
         await expect(commentRepository.addComment({})).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
         await expect(commentRepository.getCommentById('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+        await expect(commentRepository.verifyCommentIsExist('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+        await expect(commentRepository.verifyCommentAccess('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+        await expect(commentRepository.deleteCommentById('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
     })
 })
