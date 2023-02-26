61b7af80d9862ba18da46b3329eacaa0a6d610ff
rzkyadhi <rizkyadhi081@gmail.com>
2023-02-26 13:58:15 +0700

    Editing src/Infrastructures/repository/CommentRepositoryPostgres and TDD Tested

--- src/Infrastructures/repository/CommentRepositoryPostgres.js
+++ src/Infrastructures/repository/CommentRepositoryPostgres.js
@@ -1,4 +1,5 @@
 const NotFoundError = require('../../Commons/exceptions/NotFoundError')
+const AuthorizationError = require('../../Commons/exceptions/AuthorizationError')
 const AddedComment = require('../../Domains/comments/entities/AddedComment')
 const CommentRepository = require('../../Domains/comments/CommentRepository')
 
@@ -39,6 +40,46 @@ class CommentRepositoryPostgres extends CommentRepository {
             throw new NotFoundError('comment tidak ditemukan')
         }
         return result.rows[0]
+    }
+
+    async verifyCommentIsExist({
+        commentId,
+        threadId
+    }) {
+        const isDeleted = false
+        const query = {
+            text: 'SELECT 1 from comments WHERE id = $1 AND thread_id = $2 AND is_deleted = $3',
+            values: [commentId, threadId, isDeleted]
+        }
+        const result = await this._pool.query(query)
+        if (!result.rows.length) {
+            throw new NotFoundError('comment tidak ditemukan')
+        }
+    }
+
+    async verifyCommentAccess({
+        commentId,
+        owner
+    }) {
+        const query = {
+            text: 'SELECT 1 FROM comments WHERE id = $1 AND owner = $2',
+            values: [commentId, owner]
+        }
+        const result = await this._pool.query(query)
+        if (!result.rows.length) {
+            throw new AuthorizationError('anda tidak memiliki access untuk comment ini')
+        }
+    }
+
+    async deleteCommentById(commentId) {
+        const query = {
+            text: 'UPDATE comments SET is_deleted = TRUE WHERE id = $1 RETURNING id',
+            values: [commentId]
+        }
+        const result = await this._pool.query(query)
+        if (!result.rows.length) {
+            throw new NotFoundError('tidak dapat menghapus comment karena comment tidak ditemukan')
+        }
     }
 }
 
--- src/Infrastructures/repository/_test/CommentRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/CommentRepositoryPostgres.test.js
@@ -3,6 +3,7 @@ const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper')
 const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper')
 const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper')
 const NotFoundError = require('../../../Commons/exceptions/NotFoundError')
+const AuthorizationError = require('../../../Commons/exceptions/AuthorizationError')
 const AddComment = require('../../../Domains/comments/entities/AddComment')
 const AddedComment = require('../../../Domains/comments/entities/AddedComment')
 const pool = require('../../database/postgres/pool')
@@ -18,7 +19,7 @@ describe('CommentRepositoryPostgres', () => {
 
     afterAll(async () => {
         await pool.end()
-    })  
+    })
 
     describe('addComment function', () => {
         it('should persist add comment and return added comment correctly', async () => {
@@ -136,6 +137,264 @@ describe('CommentRepositoryPostgres', () => {
 
             // Assert
             expect(comment).toStrictEqual(addComment)
+        })
+    })
+
+    describe('verifyCommentIsExist function', () => {
+        it('should throw NotFoundError when comment not found', async () => {
+            // Arrange & Action addUser
+            await UsersTableTestHelper.addUser({
+                id: 'user-123',
+                username: 'dicoding',
+                password: 'secret',
+                fullname: 'Dicoding Indonesia'
+            })
+
+            // Arrange & Action addThread
+            await ThreadsTableTestHelper.addThread({
+                id: 'thread-123',
+                title: 'Menjadi Back-End Developer Expert',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                owner: 'user-123'
+            })
+
+            // Arrange & Action addComment
+            await CommentsTableTestHelper.addComment({
+                id: 'comment-find',
+                thread_id: 'thread-123',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                owner: 'user-123'
+            })
+
+            // Arrange
+            const commentRepositoryPostgres = new CommentRepositoryPostgres(pool, {})
+
+            // Action & Assert
+            await expect(commentRepositoryPostgres.verifyCommentIsExist({
+                    commentId: 'comment-920192',
+                    threadId: 'thread-920192'
+                }))
+                .rejects
+                .toThrowError(NotFoundError)
+        })
+        it('should throw NotFoundError when comment already deleted', async () => {
+            // Arrange & Action addUser
+            await UsersTableTestHelper.addUser({
+                id: 'user-123',
+                username: 'dicoding',
+                password: 'secret',
+                fullname: 'Dicoding Indonesia'
+            })
+
+            // Arrange & Action addThread
+            await ThreadsTableTestHelper.addThread({
+                id: 'thread-123',
+                title: 'Menjadi Back-End Developer Expert',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                owner: 'user-123'
+            })
+
+            // Arrange & Action addComment
+            await CommentsTableTestHelper.addComment({
+                id: 'comment-find',
+                thread_id: 'thread-123',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                owner: 'user-123',
+                is_deleted: true
+            })
+
+            // Arrange
+            const commentRepositoryPostgres = new CommentRepositoryPostgres(pool, {})
+
+            // Action & Assert
+            await expect(commentRepositoryPostgres.verifyCommentIsExist({
+                    commentId: 'comment-find',
+                    threadId: 'thread-123'
+                }))
+                .rejects
+                .toThrowError(NotFoundError)
+        })
+        it('should resolve function correctly', async () => {
+            // Arrange & Action addUser
+            await UsersTableTestHelper.addUser({
+                id: 'user-123',
+                username: 'dicoding',
+                password: 'secret',
+                fullname: 'Dicoding Indonesia'
+            })
+
+            // Arrange & Action addThread
+            await ThreadsTableTestHelper.addThread({
+                id: 'thread-123',
+                title: 'Menjadi Back-End Developer Expert',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                owner: 'user-123'
+            })
+
+            // Arrange & Action addComment
+            await CommentsTableTestHelper.addComment({
+                id: 'comment-find',
+                thread_id: 'thread-123',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                owner: 'user-123'
+            })
+
+            // Arrange
+            const commentRepositoryPostgres = new CommentRepositoryPostgres(pool, {})
+
+            // Action & Assert
+            await expect(commentRepositoryPostgres.verifyCommentIsExist({
+                    commentId: 'comment-find',
+                    threadId: 'thread-123'
+                }))
+                .resolves
+                .toBeUndefined()
+        })
+    })
+
+    describe('verifyCommentAccess function', () => {
+        it('should throw AuthorizationError when user have no authorization', async () => {
+            // Arrange & Action addUser
+            await UsersTableTestHelper.addUser({
+                id: 'user-123',
+                username: 'dicoding',
+                password: 'secret',
+                fullname: 'Dicoding Indonesia'
+            })
+
+            // Arrange & Action addThread
+            await ThreadsTableTestHelper.addThread({
+                id: 'thread-123',
+                title: 'Menjadi Back-End Developer Expert',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                owner: 'user-123'
+            })
+
+            // Arrange & Action addComment
+            await CommentsTableTestHelper.addComment({
+                id: 'comment-find',
+                thread_id: 'thread-123',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                owner: 'user-123'
+            })
+
+            // Arrange
+            const commentRepositoryPostgres = new CommentRepositoryPostgres(pool, {})
+
+            // Action & Assert
+            await expect(commentRepositoryPostgres.verifyCommentAccess({
+                    commentId: 'comment-find',
+                    owner: 'owner-not-found'
+                }))
+                .rejects
+                .toThrowError(AuthorizationError)
+        })
+        it('should resolve function correctly', async () => {
+            // Arrange & Action addUser
+            await UsersTableTestHelper.addUser({
+                id: 'user-123',
+                username: 'dicoding',
+                password: 'secret',
+                fullname: 'Dicoding Indonesia'
+            })
+
+            // Arrange & Action addThread
+            await ThreadsTableTestHelper.addThread({
+                id: 'thread-123',
+                title: 'Menjadi Back-End Developer Expert',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                owner: 'user-123'
+            })
+
+            // Arrange & Action addComment
+            await CommentsTableTestHelper.addComment({
+                id: 'comment-find',
+                thread_id: 'thread-123',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                owner: 'user-123'
+            })
+
+            // Arrange
+            const commentRepositoryPostgres = new CommentRepositoryPostgres(pool, {})
+
+            // Action & Assert
+            await expect(commentRepositoryPostgres.verifyCommentAccess({
+                    commentId: 'comment-find',
+                    owner: 'user-123'
+                }))
+                .resolves
+                .toBeUndefined()
+        })
+    })
+
+    describe('deleteCommentById function', () => {
+        it('should throw NotFoundError when comment not found', async () => {
+            // Arrange & Action addUser
+            await UsersTableTestHelper.addUser({
+                id: 'user-123',
+                username: 'dicoding',
+                password: 'secret',
+                fullname: 'Dicoding Indonesia'
+            })
+
+            // Arrange & Action addThread
+            await ThreadsTableTestHelper.addThread({
+                id: 'thread-123',
+                title: 'Menjadi Back-End Developer Expert',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                owner: 'user-123'
+            })
+
+            // Arrange & Action addComment
+            await CommentsTableTestHelper.addComment({
+                id: 'comment-find',
+                thread_id: 'thread-123',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                owner: 'user-123'
+            })
+
+            // Arrange
+            const commentRepositoryPostgres = new CommentRepositoryPostgres(pool, {})
+
+            // Action & Assert
+            await expect(commentRepositoryPostgres.deleteCommentById('comment-920192'))
+                .rejects
+                .toThrowError(NotFoundError)
+        })
+        it('should resolve function correctly', async () => {
+            // Arrange & Action addUser
+            await UsersTableTestHelper.addUser({
+                id: 'user-123',
+                username: 'dicoding',
+                password: 'secret',
+                fullname: 'Dicoding Indonesia'
+            })
+
+            // Arrange & Action addThread
+            await ThreadsTableTestHelper.addThread({
+                id: 'thread-123',
+                title: 'Menjadi Back-End Developer Expert',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                owner: 'user-123'
+            })
+
+            // Arrange & Action addComment
+            await CommentsTableTestHelper.addComment({
+                id: 'comment-find',
+                thread_id: 'thread-123',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                owner: 'user-123'
+            })
+
+            // Arrange
+            const commentRepositoryPostgres = new CommentRepositoryPostgres(pool, {})
+
+            // Action & Assert
+            await expect(commentRepositoryPostgres.deleteCommentById('comment-find')).resolves.toBeUndefined()
+
+            // Assert
+            const comment = await CommentsTableTestHelper.findCommentById('comment-find')
+            expect(comment[0].is_deleted).toEqual(true)
         })
     })
 })
