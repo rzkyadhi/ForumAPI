0860499aa646c463d32d71e07024a0a341379e39
rzkyadhi <rizkyadhi081@gmail.com>
2023-02-28 22:30:32 +0700

    Editing tests/CommentsTableTestHelper

--- tests/CommentsTableTestHelper.js
+++ tests/CommentsTableTestHelper.js
@@ -7,11 +7,12 @@ const CommentsTableTestHelper = {
         thread_id = 'thread-123',
         content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         owner = 'owner-123',
+        date = '',
         is_deleted = false
     }) {
         const query = {
-            text: 'INSERT INTO comments (id, thread_id, content, owner, is_deleted) VALUES($1, $2, $3, $4, $5)',
-            values: [id, thread_id, content, owner, is_deleted]
+            text: 'INSERT INTO comments (id, thread_id, content, owner, date, is_deleted) VALUES($1, $2, $3, $4, $5, $6)',
+            values: [id, thread_id, content, owner, date, is_deleted]
         }
 
         await pool.query(query)
