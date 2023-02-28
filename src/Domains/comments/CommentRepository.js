81dfe0f05643f0c37838f6864db428a599c8ba5a
rzkyadhi <rizkyadhi081@gmail.com>
2023-02-28 22:40:57 +0700

    Editing src/Domains/comments/CommentRepository and TDD Tested

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -18,6 +18,10 @@ class CommentRepository {
     async deleteCommentById(commentId) {
         throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
     }
+
+    async getCommentsByThreadId(threadId) {
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+    }
 }
 
 module.exports = CommentRepository
