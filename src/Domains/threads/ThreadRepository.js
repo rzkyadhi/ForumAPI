9f14841e3230b66ad7bdb35d3f0e45095e599550
rzkyadhi <rzkyadhi081@gmail.com>
2023-03-19 05:56:34 +0700

    Editing src/Domains/threads/ThreadRepository and test file

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -6,6 +6,10 @@ class ThreadRepository {
     async getThreadById(id) {
         throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED')
     }
+
+    async getThreadDetailById(id) {
+        throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+    }
 }
 
 module.exports = ThreadRepository
