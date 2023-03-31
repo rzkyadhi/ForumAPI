019f173724eee73e4c8e86223f8114ff5129ff73
rzkyadhi <rzkyadhi081@gmail.com>
2023-04-01 06:55:17 +0700

    pass say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -17,6 +17,14 @@ const createServer = async (container) => {
     plugin: Jwt,
   }]);
 
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Hello world!',
+    }),
+  });
+
   server.auth.strategy('forumapi_jwt', 'jwt', {
     keys: process.env.ACCESS_TOKEN_KEY,
     verify: {
